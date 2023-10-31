ab4bf944393134f803728ed75bf49543e69e25b1
Titus Kovalik <titus.kovalik@tlgcohort.com>
2023-10-31 10:42:51 -0700

    red line begone

---
+++ Lab01.2/src/com/entertainment/Television.java
@@ -0,0 +1,38 @@
+package com.entertainment;
+
+class Television {
+    private String brand;
+    private int volume;
+    private Tuner tuner = new Tuner();
+
+    //ctors
+    public Television() {
+
+    }
+
+    public int getCurrentChannel() {
+        return tuner.getChannel();
+    }
+
+    public void changeChannel(int channel) {
+        tuner.setChannel(channel);
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+
+}
