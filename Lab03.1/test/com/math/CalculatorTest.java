a7a3ad0d943666ca43faeec3cdb27ac650efd86b
Titus Kovalik <titus.kovalik@tlgcohort.com>
2023-11-02 08:29:31 -0700

    Lab03.1 (Junit getting started) complete + jars)

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,13 +9,48 @@
 package com.math;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
+
+import org.junit.*;
 
 public class CalculatorTest {
+    // fixture: this is the business object(s) under test
+    private Calculator calc;
 
-  @Test
-  public void testAdd() {
-    Calculator calc = new Calculator();
-    assertEquals(5, calc.add(1, 4));  // expected, actual
-  }
+    @BeforeClass
+    public static void initializeTestRun() {
+        System.out.println("initializeTestRun");
+    }
+
+    @AfterClass
+    public static void finalizeTestRun() {
+        System.out.println("finalizeTestRun");
+    }
+
+    @Before
+    public void setUp() {
+        System.out.println("setUp");
+        calc = new Calculator();
+    }
+    @After
+    public void tearDown() {
+        System.out.println("tearDown");
+        //generally for closing files or databases that where opened by the setUp @Before
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calc.add(1, 4));  // expected, actual
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2.0, calc.divide(4,2), .001);
+    }
+
+    @Test
+    public void testIsEquals() {
+        assertTrue(calc.isEven(10));
+        assertFalse(calc.isEven(11));
+    }
+
 }
