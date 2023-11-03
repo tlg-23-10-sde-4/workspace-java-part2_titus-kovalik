3dbd74b3f2a764a153456493a2912b2b88f09b98
Titus Kovalik <titus.kovalik@tlgcohort.com>
2023-11-03 15:37:39 -0700

    Lab04.2 Writing generic types complete Lab03.3 Testing progress made

---
+++ Lab03.3/test/com/entertainment/TelevisionTest.java
@@ -0,0 +1,89 @@
+package com.entertainment;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TelevisionTest{
+    private Television tv;
+    private Television tv1;
+    @Before
+    public void setUp() {
+        tv = new Television();
+        tv1 = new Television();
+    }
+
+
+
+    @Test
+    public void compareTo_shouldNotSortByBrand_byBrandNaturalOrder() {
+        tv.setBrand("Sony");
+        tv1.setBrand("LG");
+        assertEquals(7, tv.compareTo(tv1));
+    }
+
+    @Test
+    public void compareTo_shouldSortByBrand_byBrandNaturalOrder() {
+        tv1.setBrand("LG");
+        tv.setBrand("LG");
+        assertEquals(0, tv.compareTo(tv1));
+    }
+
+    @Test(expected = InvalidChannelException.class)
+    public void changeChannel_shouldThrow_InvalidChannelException_invalid_upperBound() throws InvalidChannelException {
+        tv.changeChannel(1000);
+    }
+
+    @Test(expected = InvalidChannelException.class)
+    public void changeChannel_shouldThrow_InvalidChannelException_invalid_lowerBound() throws InvalidChannelException {
+        tv.changeChannel(0);
+    }
+
+    @Test
+    public void changeChannel_shouldStoreValue_valid_upperBound() throws InvalidChannelException {
+        tv.changeChannel(999);
+        assertEquals(999, tv.getCurrentChannel());
+    }
+
+    @Test
+    public void changeChannel_shouldStoreValue_valid_lowerBound() throws InvalidChannelException {
+        tv.changeChannel(1);
+        assertEquals(1, tv.getCurrentChannel());
+    }
+
+    @Test
+    public void setVolume_shouldThrowIllegalArgumentException_whenInvalid_upperBound() {
+        try {
+            tv.setVolume(101);
+            fail("Should have thrown IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e) {
+            String expected = "Invalid volume: 101. Allowed range: [0,100].";
+            assertEquals(expected, e.getMessage());
+        }
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void setVolume_shouldThrowIllegalArgumentException_whenInvalid_lowerBound() {
+        tv.setVolume(-1);
+    }
+
+    @Test
+    public void setVolume_shouldStoreVolume_ifValid_upperBound() {
+        tv.setVolume(100);
+        assertEquals(100, tv.getVolume());
+    }
+
+    @Test
+    public void setVolume_shouldStoreValue_whenValid_lowerBound() {
+
+        tv.setVolume(0);
+        assertEquals(0, tv.getVolume());
+    }
+
+//    @Override
+//    public int compareTo(Television o) {
+//        return 0;
+//    }
+}
--- Lab04.2/src/com/javatunes/product/ShoppingCart.java
+++ Lab04.2/src/com/javatunes/product/ShoppingCart.java
@@ -12,15 +12,30 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
-public class ShoppingCart<T> {
-  // storage for the cart's contents
-  private Collection<T> items = new ArrayList<T>();  // diamond not used here just to emphasize the T
-  
-  public void addItem(T item) {
-    items.add(item);
-  }
-  
-  public void removeItem(T item) {
-    items.remove(item);
-  }
+public class ShoppingCart<T extends Product> {
+    // storage for the cart's contents
+    private final Collection<T> items = new ArrayList<T>();  // diamond not used here just to emphasize the T
+
+    public double total() {
+        double result = 0.0;
+        for (T item : items) {
+            result += item.getPrice();
+        }
+        return result;
+    }
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public void removeItem(T item) {
+        items.remove(item);
+    }
+    public Collection<T> allItems() {
+        return Collections.unmodifiableCollection(items);
+    }
+    public int size() {
+        return items.size();
+    }
+
 }
